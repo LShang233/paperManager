8926315c2f57382b910d6f27c57ffe8dd13bfae1
xhw2333 <1311844250@qq.com>
2020-11-14 21:23:59 +0800

    搜索文献添加

--- src/views/Literature/Literature.vue
+++ src/views/Literature/Literature.vue
@@ -5,10 +5,50 @@
       <!-- <PublishDoc /> -->
       <div class="title">
         <div>已发布的文献({{ AllDoc }}篇)</div>
-        <div>
+        <div class="doc-add">
+          <div class="search-type">
+            <input
+              class="doc-attach"
+              type="radio"
+              name="group1"
+              value="1"
+              id="title"
+              @click="changeAttach"
+              checked
+            />
+            <label for="title">文章标题</label>
+            <input
+              class="doc-attach"
+              type="radio"
+              name="group1"
+              value="2"
+              id="author"
+              @click="changeAttach"
+            />
+            <label for="author">作者</label>
+            <input
+              class="doc-attach"
+              type="radio"
+              name="group1"
+              value="3"
+              id="keyword"
+              @click="changeAttach"
+            />
+            <label for="keyword">关键词</label>
+          </div>
+          <div class="doc-search">
+            <Input
+              style="width: 400px"
+              search
+              enter-button
+              placeholder="请输入关键词搜索文献"
+              v-model="searchContent"
+              @on-search="searchDoc(1)"
+            />
+          </div>
           <router-link to="/NewLiterature">
-          <Button type="primary" ghost>+添加文献</Button>
-        </router-link>
+            <Button type="primary" ghost>+添加文献</Button>
+          </router-link>
         </div>
       </div>
       <Table :columns="columns1" :data="docList">
@@ -19,7 +59,7 @@
           >
         </template>
       </Table>
-      <Page class="page" :total="AllDoc" @on-change="changePage" />
+      <Page class="page" :total="docNum" @on-change="changePage" />
     </div>
   </div>
 </template>
@@ -63,7 +103,10 @@ export default {
       ],
       docList: [],
       publishDoc: "",
-      AllDoc: 0,
+      AllDoc: 0, //已发布文献总数
+      docNum: 0, //文献数量，分页用到
+      isSearch: false, //是否是搜索出来的文献
+      attach: 1, //搜索附加条件
     };
   },
 
@@ -87,9 +130,10 @@ export default {
           if (res.data.code == 1) {
             this.$Message.success("获取成功");
             this.AllDoc = res.data.data.total;
+            this.docNum = res.data.data.total;
             this.docList = res.data.data.list;
           } else {
-            this.$Message.error('获取失败');
+            this.$Message.error("获取失败");
           }
         })
         .catch((err) => {
@@ -139,11 +183,58 @@ export default {
         });
     },
 
+    //搜索文献
+    searchDoc(pageNum) {
+      const docsmsg = this.$Message.loading({
+        content: "搜索中...",
+        duration: 0,
+      });
+      this.$http
+        .get(this.domain + "doc", {
+          params: {
+            pageNum: pageNum,
+            pageSize: 9,
+            input: this.searchContent,
+            attach: this.attach, //以标题来搜索
+          },
+        })
+        .then((res) => {
+          setTimeout(docsmsg, 0);
+          if (res.data.code == 1) {
+            this.$Message.success("搜索结果已呈现");
+            this.docList = res.data.data.list;
+            this.docNum = res.data.data.total;
+            this.isSearch = true;
+          } else {
+            this.$Message.error("搜索失败");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          setTimeout(docsmsg, 0);
+        });
+    },
+
+    //选择的附加条件
+    changeAttach(){
+      let attachs = document.getElementsByClassName('doc-attach');
+      for(let i=0;i<attachs.length;i++){
+        if(attachs[i].checked == true){
+          this.attach = attachs[i].value;
+        }
+      }
+    },
+
     /* 分页 */
     changePage(index) {
       index = parseInt(index);
 
-      this.getPublishDoc(parseInt(index));
+      //根据是否有搜索来进行翻页
+      if (this.isSearch) {
+        this.searchDoc(parseInt(index));
+      } else {
+        this.getPublishDoc(parseInt(index));
+      }
     },
   },
 
@@ -166,6 +257,33 @@ export default {
       font-weight: bold;
       color: #808080;
       border-bottom: 1px dashed #808080;
+    }
+
+    .doc-add {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 10px;
+      font-size: 14px;
+
+      .search-type {
+        justify-self: flex-start;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-right: 10px;
+        > * {
+          margin: 0 5px;
+          cursor: pointer;
+        }
+        > *:hover {
+          color: #2d8cf0;
+        }
+      }
+
+      .doc-search {
+        margin-right: 50px;
+      }
     }
 
     .page {
