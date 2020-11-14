27ca621e4fbd6f0c869e8869a7cdcc78cb2c41c1
xhw2333 <1311844250@qq.com>
2020-11-14 15:02:36 +0800

    增加添加文献功能

--- src/views/LiteratureDetail/LiteratureDetail.vue
+++ src/views/LiteratureDetail/LiteratureDetail.vue
@@ -44,7 +44,7 @@
             type="textarea"
             :autosize="{ maxRows: 5, minRows: 5 }"
             v-model="doc.abstractText"
-          ></Input>
+          />
         </div>
         <div>
           <Button @click="returnBack" style="margin-right: 20px">返回</Button>
@@ -87,7 +87,7 @@ export default {
         this.doc.publishTime = time;
         this.doc.fromJournal = fromJournal;
         this.doc.paperType = paperType;
-        this.periodicalId = periodicalId;
+        this.doc.periodicalId = periodicalId;
       }
     },
 
@@ -104,10 +104,23 @@ export default {
     getTheDoc() {
       let id = this.$route.query.id;
       //   console.log(id);
-
+       const docsmsg = this.$Message.loading({
+        content: "Loading...",
+        duration: 0,
+      });
       this.$http.get(this.domain + `doc/${id}`).then((res) => {
+        setTimeout(docsmsg, 0);
         console.log(res.data);
-        this.doc = res.data.data;
+        if(res.data.code == 1){
+          this.$Message.success("获取成功");
+           this.doc = res.data.data;
+        } else {
+          this.$Message.error('获取失败');
+        }
+       
+      }).catch(err=>{
+        console.log(err);
+        setTimeout(docsmsg, 0);
       });
     },
 
@@ -126,7 +139,7 @@ export default {
       this.$http
         .put(
           this.domain +
-            `docs?id=${this.doc.id}&title=${this.doc.title}&keyword=${this.doc.keyword}&author=${this.doc.author}&publishTime=${this.doc.publishTime}&fromJournal=${this.doc.fromJournal}&paperType=${this.doc.paperType}&periodicalId=${this.periodicalId}&abstractText=${this.doc.abstractText}`,
+            `docs?id=${this.doc.id}&title=${this.doc.title}&keyword=${this.doc.keyword}&author=${this.doc.author}&publishTime=${this.doc.publishTime}&fromJournal=${this.doc.fromJournal}&paperType=${this.doc.paperType}&periodicalId=${this.doc.periodicalId}&abstractText=${this.doc.abstractText}`,
           {},
           {
             headers: {
@@ -148,6 +161,7 @@ export default {
         })
         .catch((err) => {
           console.log(err);
+          this.$Message.error("修改失败");
         });
     },
 
--- src/views/ReviewLiterature/ReviewLiterature.vue
+++ src/views/ReviewLiterature/ReviewLiterature.vue
@@ -1,112 +1,121 @@
 <template>
-<div class="reviewliterature">
-    <Banner :title="'审核文献'"/>
+  <div class="reviewliterature">
+    <Banner :title="'审核文献'" />
     <div class="message-container">
-        <div class="docMessage-list" v-for="(item, index) in docMessage" :key="index">
-            <div class="doc-intro">
-                <strong>{{item.author}}(作者)</strong>
-                的投稿文献
-                <strong>《{{item.title}}》</strong> 
-            </div>
-            <DocDetail :docMessage="item"/>
+      <div
+        class="docMessage-list"
+        v-for="(item, index) in docMessage"
+        :key="index"
+      >
+        <div class="doc-intro">
+          <strong>{{ item.author }}(作者)</strong>
+          的投稿文献
+          <strong>《{{ item.title }}》</strong>
         </div>
+        <DocDetail :docMessage="item" />
+      </div>
+      <h3 v-show="!docMessage.length" style="text-align:center">暂无审核信息</h3>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import Banner from '../../components/Banner/Banner';
-import DocDetail from '../../components/ReviewLiterature/DocDetail'
+import Banner from "../../components/Banner/Banner";
+import DocDetail from "../../components/ReviewLiterature/DocDetail";
 
 export default {
-    name: 'ReviewLiterature',
+  name: "ReviewLiterature",
 
-    data(){
-        return {
-            docMessage: [
-                // {
-                //     author: 'xhw',
-                //     phone: '18407112001',
-                //     email: '18291210@qq.com',
-                //     title: '如何从菜鸟变为大神',
-                //     keyword: '励志,大神',
-                //     abstractText: '这是菜鸡变为大神的一个故事,我是你爸爸afafafafafaf',
-                // },
-                // '','','','','','','','',''
-            ],
-        }
+  data() {
+    return {
+      docMessage: [
+        // {
+        //     author: 'xhw',
+        //     phone: '18407112001',
+        //     email: '18291210@qq.com',
+        //     title: '如何从菜鸟变为大神',
+        //     keyword: '励志,大神',
+        //     abstractText: '这是菜鸡变为大神的一个故事,我是你爸爸afafafafafaf',
+        // },
+        // '','','','','','','','',''
+      ],
+    };
+  },
+  components: {
+    Banner,
+    DocDetail,
+  },
+  methods: {
+    //获取审核消息
+    getDocMessage() {
+      const docsmsg = this.$Message.loading({
+        content: "Loading...",
+        duration: 0,
+      });
+      this.$http.get(this.domain + "cons").then((res) => {
+          setTimeout(docsmsg,0);
+        this.docMessage = res.data.data;
+        console.log(this.docMessage);
+      }).catch(err=>{
+          console.log(err);
+          setTimeout(docsmsg,0);
+      });
     },
-    components: {
-        Banner,
-        DocDetail
-    },
-    methods: {
-        //获取审核消息
-        getDocMessage(){
-            this.$http.get(this.domain + "cons").then(res=>{
-                
-                this.docMessage = res.data.data;
-                console.log(this.docMessage);
-            })
-        },
 
-        //删除数据
-        deleteDoc(title){
-            let index = this.getIndex(title);
-            this.docMessage.splice(index,1);
-        },
+    //删除数据
+    deleteDoc(title) {
+      let index = this.getIndex(title);
+      this.docMessage.splice(index, 1);
+    },
 
-        //根据标题找索引
-        getIndex(title){
-            let Num;
-            this.docMessage.forEach((item,index)=>{
-                if(item.title == title) {
-                    Num = index;
-                }
-            });
-            return parseInt(Num);
+    //根据标题找索引
+    getIndex(title) {
+      let Num;
+      this.docMessage.forEach((item, index) => {
+        if (item.title == title) {
+          Num = index;
         }
+      });
+      return parseInt(Num);
     },
-    mounted(){
-        this.getDocMessage();
+  },
+  mounted() {
+    this.getDocMessage();
+  },
+  watch: {
+    docMessage(val, oldVal) {
+      return val;
     },
-    watch: {
-        docMessage(val,oldVal){
-            return val;
-        }
-    }
-        
-    
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .reviewliterature {
-    .message-container {
-        padding: 20px 40px;
+  .message-container {
+    padding: 20px 40px;
 
+    .docMessage-list {
+      margin: 20px 0;
+      padding: 5px 20px;
+      height: 60px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      background: #fff;
+      box-shadow: 2px 2px 10px rgba(128, 128, 128, 0.3);
+      overflow: hidden;
 
-        .docMessage-list {
-            margin: 20px 0;
-            padding: 5px 20px;
-            height: 60px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            width: 100%;
-            background: #fff;
-            box-shadow: 2px 2px 10px rgba(128,128,128,0.3);
-            overflow: hidden;
-
-            .doc-intro {
-                font-size: 16px;
-                flex: 1;
-            }
-        }
+      .doc-intro {
+        font-size: 16px;
+        flex: 1;
+      }
     }
+  }
 
-    .message-page {
-        text-align: center;
-    }
+  .message-page {
+    text-align: center;
+  }
 }
 </style>
